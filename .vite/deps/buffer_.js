3ee31bc56e3319d3221c720705c9be6f5417c83f
Sopwit <131982697+Sopwit@users.noreply.github.com>
2026-03-11 00:02:11 +0300

    chore: remove tracked build artifacts and update gitignore
    - Remove .vite/ cache directory from git tracking
    - Update .gitignore to exclude .vite/ and dist/ properly
    - Fix react/react-dom moved to dependencies from peerDependencies
    - Fix CI workflow paths (was pointing to non-existent /frontend dir)

--- .vite/deps/buffer_.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_buffer
-} from "./chunk-MEF36UWI.js";
-import "./chunk-MVEJMUOB.js";
-export default require_buffer();
--- .vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-HZQOY4LB.js";
-import "./chunk-MVEJMUOB.js";
-export default require_react();
