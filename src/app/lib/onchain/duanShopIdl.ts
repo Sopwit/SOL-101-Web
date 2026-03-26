9574979e17fa42a047ba7598a859a0309ba17f67
Sopwit <131982697+Sopwit@users.noreply.github.com>
2026-03-26 18:46:27 +0300

    feat: Add duan_shop Anchor IDL and update its import path.

--- src/app/lib/onchain/duanShopIdl.ts
+++ src/app/lib/onchain/duanShopIdl.ts
@@ -1,4 +1,4 @@
-import duanShopIdl from '../../../../target/idl/duan_shop.json';
+import duanShopIdl from './duan_shop.json';
 
 export const DUAN_SHOP_PROGRAM_ID = duanShopIdl.address;
 export const DUAN_SHOP_IDL = duanShopIdl;
